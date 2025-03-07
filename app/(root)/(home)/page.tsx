4d3aebc9fb88a1a3995889735f0bab040c6c59e4
Revan186 <aragas1245@gmail.com>
2025-03-07 21:08:50 +0400

    client time

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,10 @@
+'use client'
+
 import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
   const now = new Date();
-  const yiop = ''
+
 
   const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
   const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
