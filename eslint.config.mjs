944d90207538f64629d59de1895465c32098748a
Revan186 <aragas1245@gmail.com>
2025-03-07 20:00:34 +0400

    typescript ignored

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+{
+	"extends": ["next/core-web-vitals"]
+}
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", ),
-];
+// import { FlatCompat } from "@eslint/eslintrc"
+// import { dirname } from "path"
+// import { fileURLToPath } from "url"
 
-export default eslintConfig;
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = dirname(__filename)
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// })
+
+// const eslintConfig = [
+//   {
+//     files: ["**/*"],
+//     ignores: ["**/*"],
+//     rules: {
+//       "@typescript-eslint/no-explicit-any": "off",
+//       "@typescript-eslint/no-unused-vars": "off"
+//     }
+//   },
+//   ...compat.extends("next/core-web-vitals"),
+// ]
+
+// export default eslintConfig
+
